36e5452a031f90cd5ab2388efebef2b8c924e202
Annmolkumar <anmolaaryan@gmail.com>
2019-07-09 11:54:57 -0400

    trash removal

--- run_creat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-drudeDir=drude_pdbs
-psflist=$(for i in ${drudeDir}/*.psf;do echo $i | cut -f1 -d'.';done)
-#echo $psflist
-for psf in $psflist;do
-  echo " processing $psf"
-  python create_lp.py ${psf}_start.pdb $psf.psf 
-done
